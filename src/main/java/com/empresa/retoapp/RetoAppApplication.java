bc720b0c8fc8ef33fb75d9ca71e59938e5bdd7d3
nbaez001 <nbaez001@gmail.com>
2021-08-28 18:05:09 -0500

    [Creacion del proyecto]

---
+++ src/main/java/com/empresa/retoapp/RetoAppApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.retoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/retoapp/RetoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.retoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
